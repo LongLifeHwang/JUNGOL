ce7ebfc0f8fc71a2166dce25822d5007e91a6623
LongLifeHwang <Rhig0617@gmail.com>
2023-10-06 07:01:45 +0900

    LCoder_C_jungol_9008

---
+++ LCoder_C/LCoder_C_9000/jungol_9008.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf(" subject score\n");
+	printf("==============\n");
+	printf("  korean	90\n");
+	printf(" english   100\n");
+	printf("computer	80\n");
+	return 0;
+}
