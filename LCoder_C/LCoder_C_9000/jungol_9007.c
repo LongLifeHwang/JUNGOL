5d3f49f95c97948dc1a60ff20848392a7eaa06b2
LongLifeHwang <Rhig0617@gmail.com>
2023-10-06 07:00:35 +0900

    LCoder_C_jungol_9007

---
+++ LCoder_C/LCoder_C_9000/jungol_9007.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "수식을 출력하면 계산 결과가 출력됩니다.");
+	printf("%d + %d = 15\n", 10, 5, 10+5);
+	printf("내 생일은 %d년 %d월 %d일 입니다.\n", 2005, 6, 17);
+	return 0;
+}
