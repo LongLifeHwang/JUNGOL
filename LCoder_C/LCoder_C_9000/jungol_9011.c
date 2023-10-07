d1a9799992517f707074c3ffd64f64fd72810924
LongLifeHwang <Rhig0617@gmail.com>
2023-10-07 22:45:56 +0000

    jungol_9011.c

---
+++ LCoder_C/LCoder_C_9000/jungol_9011.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 10;
+	b = 20;
+	printf("%d + %d = %d\n", a, b, a+b);
+	a = 30;
+	b = 40;
+	printf("%d + %d = %d", a, b, a + b);
+	return 0;
+}
