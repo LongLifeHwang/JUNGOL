0d18c9de31bf052a307ea5aa8529997f6ba19f50
LongLifeHwang <Rhig0617@gmail.com>
2023-10-14 23:04:34 +0900

    jungol_9020

---
+++ LCoder_C/LCoder_C_9000/jungol_9020.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+// 맨 앞줄에 작성
+#include <stdio.h>
+
+int main(void) {
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int num5;
+
+	printf("5개의 수를 입력하시오. ");
+	scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
+	printf("%d %d %d %d %d", num1 + 3, num2 - 3, num3 * 3, num4 / 3, num5 % 3);
+	return 0;
+}
