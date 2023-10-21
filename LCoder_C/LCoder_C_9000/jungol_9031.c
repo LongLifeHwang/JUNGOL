7a4180c26c19b376c793bd6c71f5c7327a70785a
LongLifeHwang <Rhig0617@gmail.com>
2023-10-21 06:25:44 +0000

    jungol_9031

---
+++ LCoder_C/LCoder_C_9000/jungol_9031.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	scanf("%d %d", &a, &b);
+	if (a > b)
+	{
+		printf("입력받은 수 중 큰 수는 %d이고 작은 수는 %d입니다.", a, b);
+	}
+	else
+	{
+		printf("입력받은 수 중 큰 수는 %d이고 작은 수는 %d입니다.", b, a);
+	}
+	return 0;
+}
