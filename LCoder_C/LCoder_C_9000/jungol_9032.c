1382cd7189d76d22f5dfe91106792225f2438397
LongLifeHwang <Rhig0617@gmail.com>
2023-10-22 20:54:08 +0900

    jungol_9032

---
+++ LCoder_C/LCoder_C_9000/jungol_9032.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	printf("점수를 입력하세요. ");
+	scanf("%d", &a);
+	if (a >= 80)
+	{
+		printf("축하합니다. 합격입니다.");
+	}
+	else
+	{
+		printf("불합격입니다.");
+	}
+	return 0;
+}
