b42700b9a0d7248d3fea909ea5eb8a751674556b
LongLifeHwang <Rhig0617@gmail.com>
2023-10-23 14:25:01 +0900

    jungol_9038

---
+++ LCoder_C/LCoder_C_9000/jungol_9038.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	scanf("%d %d", &a, &b);
+	if (a > b)
+	{
+		printf("%d", a);
+	}
+	else
+	{
+		printf("%d", b);
+	}
+	return 0;
+}
