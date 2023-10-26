22250d1a30a6fd67c0c085ba81c4bf1a20da5bc9
LongLifeHwang <Rhig0617@gmail.com>
2023-10-26 18:00:11 +0900

    jungol 9046

---
+++ LCoder_C/LCoder_C_9000/jungol_9046.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int i;
+	
+	i = 1;
+	scanf("%d", &n);
+	while (i <= n)
+	{
+		printf("C언어 프로그래밍\n");
+		i++;
+	}
+	return 0;
+}
