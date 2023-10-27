a003d229ffe4b9c9edcb53df0ec92d91ef40920f
LongLifeHwang <Rhig0617@gmail.com>
2023-10-27 18:26:24 +0900

    jungol_9053

---
+++ LCoder_C/LCoder_C_9000/jungol_9053.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 1; i <= 5; i++)
+	{
+		printf("%d %d %d %d %d\n", i, i, i, i, i);
+	}
+	printf("\n");
+	for (int i = 1; i <= 5; i++)
+	{
+		printf("1 2 3 4 5\n");
+	}
+	return 0;
+}
