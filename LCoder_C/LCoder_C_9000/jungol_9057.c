1a0491d79bf8044ef8b3c69819d7a99577c59531
LongLifeHwang <Rhig0617@gmail.com>
2023-10-29 11:30:33 +0900

    jungol 9057

---
+++ LCoder_C/LCoder_C_9000/jungol_9057.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	
+	a = 1;
+	for (int i = 1; i <= 3; i++)
+	{
+		while (a <= i)
+		{
+			printf("*");
+			a++;
+		}
+		a = 1;
+		printf("\n");
+	}
+	for (int i = 3; i >= 1; i--)
+	{
+		while (a <= i)
+		{
+			printf("*");
+			a++;
+		}
+		a = 1;
+		printf("\n");
+	}
+	return 0;
+}
