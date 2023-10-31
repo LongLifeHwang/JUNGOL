b8fc3c199e5d0e2a84d85dc6e4467436c2ff49ca
LongLifeHwang <Rhig0617@gmail.com>
2023-10-31 11:25:21 +0900

    jungol_9063

---
+++ LCoder_C/LCoder_C_9000/jungol_9063.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char c[4];
+	char c1[1024];
+
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%s", c);
+		strcat(c, " ");
+		strcat(c1, c);
+	}
+	printf("%s", c1);
+	return 0;
+}
