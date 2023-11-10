c4f87f17e0d370d5cb02d90cbacd479e5598afc8
LongLifeHwang <Rhig0617@gmail.com>
2023-11-10 16:53:36 +0900

    jungol_9088

---
+++ LCoder_C/LCoder_C_9000/jungol_9088.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int j_9088_1(int a, int b, char n[1])
+{
+	if ((int)n[0] == 43)
+	{
+		return a + b;
+	}
+	else if ((int)n[0] == 45)
+	{
+		return a - b;
+	}
+	else if ((int)n[0] == 42)
+	{
+		return a * b;
+	}
+	else if ((int)n[0] == 47)
+	{
+		return a / b;
+	}
+}
+
+int main(void)
+{
+	char n[2];
+	int a;
+	int b;
+
+	scanf("%d %s %d", &a, &n, &b);
+	printf("%d %s %d = %d", a, n, b, j_9088_1(a, b, n));
+	return 0;
+}
