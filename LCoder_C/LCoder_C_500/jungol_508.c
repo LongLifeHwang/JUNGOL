c69a638b8ac1acf1453d359b654ec65f16e0cf85
LongLifeHwang <Rhig0617@gmail.com>
2023-11-23 17:42:11 +0900

    jungol_508.c

---
+++ LCoder_C/LCoder_C_500/jungol_508.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%10s", "item");
+	printf("%10s", "count");
+	printf("%10s\n", "price");
+	printf("%10s", "pen");
+	printf("%10s", "20");
+	printf("%10s\n", "100");
+	printf("%10s", "note");
+	printf("%10s", "5");
+	printf("%10s\n", "95");
+	printf("%10s", "eraser");
+	printf("%10s", "110");
+	printf("%10s\n", "97");
+	return 0;
+}
