cb740da6b3e14538d4a65c0e57eb27a2817875ca
LongLifeHwang <Rhig0617@gmail.com>
2024-03-05 21:23:54 +0900

    jungol_596.c

---
+++ LCoder_C/LCoder_C_500/jungol_596.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int a;
+int len;
+char c[100];
+
+int main() {
+	scanf("%s", c);
+	scanf("%d", &a);
+	len = strlen(c);
+	for (int i = len - 1; i >= len - a && i>=0; i--)
+	{
+		printf("%c", c[i]);
+	}
+	return 0;
+}
