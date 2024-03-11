86e889636d5cb55d6080f6f736ea328ac3e9c92b
LongLifeHwang <Rhig0617@gmail.com>
2024-03-11 18:36:38 +0900

    jungol_599.c

---
+++ LCoder_C/LCoder_C_500/jungol_599.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int len;
+char c[100];
+
+int main()
+{
+	scanf("%s", c);
+	len = strlen(c);
+	for (int i = 0; i < len; i++)
+	{
+		if ((int)c[i] >= 65 && (int)c[i] <= 90)
+		{
+			printf("%c", c[i]);
+		}
+		else if ((int)c[i] >= 97 && (int)c[i] <= 122)
+		{
+			c[i] -= 32;
+			printf("%c", c[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return 0;
+}
