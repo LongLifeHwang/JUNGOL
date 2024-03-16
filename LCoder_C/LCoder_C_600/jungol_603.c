029681ed3741a0d6cc4051d442008ef623553da8
Ingyu Hwang <inghwang@cx1r4s5.42seoul.kr>
2024-03-16 16:56:21 +0900

    jungol_603.c

---
+++ LCoder_C/LCoder_C_600/jungol_603.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[50];
+	char* ptr_ary[50] = { 0, };
+	char* ptr;
+	int i;
+	
+	i = 0;
+	scanf("%[^\n]s", str);
+	ptr = strtok(str, " ");
+	while (ptr != NULL) {
+		ptr_ary[i] = ptr;
+		i++;
+		ptr = strtok(NULL, " ");
+	}
+	for (int j = 0; j < i; j+=2)
+	{
+		printf("%s\n", ptr_ary[j]);
+	}
+	return 0;
+}
