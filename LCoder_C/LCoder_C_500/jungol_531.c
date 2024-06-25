c39fae00d4d146de6cf5ce7179d9c01f33bd1aa4
LongLifeHwang <Rhig0617@gmail.com>
2024-06-25 20:34:59 +0000

    jungol_531.c

--- LCoder_C/LCoder_C_500/jungol_531.c
+++ LCoder_C/LCoder_C_500/jungol_531.c
@@ -7,24 +7,14 @@ int main(void)
 
 	scanf("%d", &a);
 	if (a > 88.45)
-	{
 		printf("Heavyweight");
-	}
 	else if (a > 72.57)
-	{
 		printf("Cruiserweight");
-	}
 	else if (a > 61.23)
-	{
 		printf("Middleweight");
-	}
 	else if (a > 50.80)
-	{
 		printf("Lightweight");
-	}
 	else
-	{
 		printf("Flyweight");
-	}
 	return 0;
 }
