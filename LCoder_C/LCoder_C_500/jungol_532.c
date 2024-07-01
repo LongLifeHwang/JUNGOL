3ef04589537719278772d417d215edf501f03214
LongLifeHwang <Rhig0617@gmail.com>
2024-07-01 22:49:32 +0900

    jungol_532.c

--- LCoder_C/LCoder_C_500/jungol_532.c
+++ LCoder_C/LCoder_C_500/jungol_532.c
@@ -8,16 +8,10 @@ int main(void)
 
 	scanf("%f %f", &a, &b);
 	if (a >= 4 && b >= 4)
-	{
 		printf("A");
-	}
 	else if (a >= 3 && b >= 3)
-	{
 		printf("B");
-	}
 	else
-	{
 		printf("C");
-	}
 	return 0;
 }
