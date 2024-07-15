e5df8bbbcad4ac3403a0b36ee156813ab48f3b13
LongLifeHwang <Rhig0617@gmail.com>
2024-07-15 23:23:11 +0000

    jungol_585.c

--- LCoder_C/LCoder_C_500/jungol_585.c
+++ LCoder_C/LCoder_C_500/jungol_585.c
@@ -19,9 +19,7 @@ void j_585_1(int data[10])
 			}
 		}
 		for (int z = 0; z < data_size; z++)
-		{
 			printf("%d ", data[z]);
-		}
 		printf("\n");
 	}
 }
@@ -31,9 +29,7 @@ int main(void)
 	int arr[data_size] = { 0, };
 
 	for (int i = 0; i < 10; i++)
-	{
 		scanf("%d", &arr[i]);
-	}
 	j_585_1(arr);
 	return 0;
 }
