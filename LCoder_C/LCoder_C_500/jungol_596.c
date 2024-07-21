c392202e940cab39a6c6a986484828b8e64cf6f8
LongLifeHwang <Rhig0617@gmail.com>
2024-07-21 15:41:53 +0000

    jungol_596.c

--- LCoder_C/LCoder_C_500/jungol_596.c
+++ LCoder_C/LCoder_C_500/jungol_596.c
@@ -6,13 +6,12 @@ int a;
 int len;
 char c[100];
 
-int main() {
+int main()
+{
 	scanf("%s", c);
 	scanf("%d", &a);
 	len = strlen(c);
 	for (int i = len - 1; i >= len - a && i>=0; i--)
-	{
 		printf("%c", c[i]);
-	}
 	return 0;
 }
