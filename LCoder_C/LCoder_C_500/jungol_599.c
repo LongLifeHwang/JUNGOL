9f127a32beb27dd656f839a36f644fa8cb4ab5ed
LongLifeHwang <Rhig0617@gmail.com>
2024-07-25 12:41:19 +0000

    jungol_599.c

--- LCoder_C/LCoder_C_500/jungol_599.c
+++ LCoder_C/LCoder_C_500/jungol_599.c
@@ -13,18 +13,14 @@ int main()
 	for (int i = 0; i < len; i++)
 	{
 		if ((int)c[i] >= 65 && (int)c[i] <= 90)
-		{
 			printf("%c", c[i]);
-		}
 		else if ((int)c[i] >= 97 && (int)c[i] <= 122)
 		{
 			c[i] -= 32;
 			printf("%c", c[i]);
 		}
 		else
-		{
 			continue;
-		}
 	}
 	return 0;
 }
