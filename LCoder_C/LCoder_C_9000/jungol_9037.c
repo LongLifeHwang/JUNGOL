687bed6542d8c3bbecb089a0276615c5e198f85c
LongLifeHwang <Rhig0617@gmail.com>
2024-07-27 12:41:19 +0000

    jungol_9037.c

--- LCoder_C/LCoder_C_9000/jungol_9037.c
+++ LCoder_C/LCoder_C_9000/jungol_9037.c
@@ -8,24 +8,14 @@ int main(void)
 	printf("점수를 입력하세요. ");
 	scanf("%d", &b);
 	if (b >= 90)
-	{
 		printf("A");
-	}
 	else if (b >= 80)
-	{
 		printf("B");
-	}
 	else if (b >= 70)
-	{
 		printf("C");
-	}
 	else if (b >= 60)
-	{
 		printf("D");
-	}
 	else
-	{
 		printf("F");
-	}
 	return 0;
 }
