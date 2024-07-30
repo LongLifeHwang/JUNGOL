288060baa22eaada0365f6b4579d8bc85c0755d6
LongLifeHwang <Rhig0617@gmail.com>
2024-07-30 21:41:19 +0000

    jungol_9031.c

--- LCoder_C/LCoder_C_9000/jungol_9031.c
+++ LCoder_C/LCoder_C_9000/jungol_9031.c
@@ -8,12 +8,8 @@ int main(void)
 
 	scanf("%d %d", &a, &b);
 	if (a > b)
-	{
 		printf("입력받은 수 중 큰 수는 %d이고 작은 수는 %d입니다.", a, b);
-	}
 	else
-	{
 		printf("입력받은 수 중 큰 수는 %d이고 작은 수는 %d입니다.", b, a);
-	}
 	return 0;
 }
