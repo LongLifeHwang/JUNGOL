b4f922e0789e659af7a2c6cb8c5d2bb92c9891c1
LongLifeHwang <Rhig0617@gmail.com>
2024-08-04 21:31:41 +0000

    jungol_9036.c

--- LCoder_C/LCoder_C_9000/jungol_9036.c
+++ LCoder_C/LCoder_C_9000/jungol_9036.c
@@ -9,16 +9,10 @@ int main(void)
 	printf("숫자를 선택하세요. ");
 	scanf("%d", &b);
 	if (b == 1)
-	{
 		printf("삽입을 선택하셨습니다.");
-	}
 	else if (b == 2)
-	{
 		printf("수정을 선택하셨습니다.");
-	}
 	else
-	{
 		printf("삭제을 선택하셨습니다.");
-	}
 	return 0;
 }
